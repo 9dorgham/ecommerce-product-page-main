c1494803e99bf40d89e98ee27514e53d6f58fb74
9dorgham <am8505841@gmail.com>
2023-12-07 10:11:20 +0200

    0.6v

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
-const nextButton = document.querySelector('.product-carousel .next')
-const previousButton = document.querySelector('.product-carousel .previous')
-const closeButton = document.querySelector('.product-carousel .close')
+const nextButton = document.querySelector('.container .next')
+const previousButton = document.querySelector('.container .previous')
+const closeButton = document.querySelector('.container .close')
 
-const carouselItems = document.querySelector('.product-carousel')
+const carouselItems = document.querySelector('.carousel')
 
 
-const state = {
-    currentStory: carouselItems.querySelector('[data-order="0"]')
+class Carousel  {
+    constructor(item) {
+        this.currentItem = item
+        this.index = this.currentItem.dataset.order
+    }
+
+
+    gotNex() {
+        console.log(this.index);
+        if (this.index == 3) return
+
+        this.currentItem.nextElementSibling.scrollIntoView({behavior: 'smooth'});
+        this.currentItem = this.currentItem.nextElementSibling
+    }
+
+
+    goPrev() {
+        if (this.index == 0) return;
+
+        this.currentItem.previousElementSibling.scrollIntoView({behavior: 'smooth'})
+        this.currentItem = this.currentItem.previousElementSibling
+    }
 }
+
+
+let carousel = new Carousel(carouselItems.firstElementChild)
 
 carouselItems.addEventListener('click', (e) => {
     const middel = carouselItems.offsetLeft + carouselItems.clientWidth / 2;
 
 
-    navigationItems(
-        e.clientX > middel
-        ? "next"
-        : "prev"
-    )
+    e.clientX > middel
+        ? carousel.gotNex()
+        : carousel.goPrev()
 
 })
 
-const navigationItems = (direction) => {
-    
-}
+
+
+// const navigationItems = (direction) => {
+//     let item = productItem.currentItem;
+//     let hasNextItem = productItem.index <= 3;
+
+
+//     if (direction == 'next') {
+//         item.nextElementSibling.scrollIntoView({behavior: 'smooth'})
+
+//         productItem.currentItem = item.nextElementSibling;
+//     }
+// }
+
+
+// console.log(productItem.currentItem);
